b6a21ff1a9714030f68f1076f85991cc064d3129
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-02-25 16:05:30 +0500

    Base58 encoded return type

---
+++ src/Base58/Result/Base58Encoded.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is a part of "cryptolabs-pk/bitcoin-php" package.
+ * https://github.com/cryptolabs-pk/bitcoin-php
+ *
+ * Copyright (c) 2019 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/cryptolabs-pk/bitcoin-php/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace CryptoLabs\Bitcoin\Base58\Result;
+
+use CryptoLabs\DataTypes\Buffer\AbstractStringType;
+
+/**
+ * Class Base58Encoded
+ * @package CryptoLabs\Bitcoin\Base58\Result
+ */
+class Base58Encoded extends AbstractStringType
+{
+}
