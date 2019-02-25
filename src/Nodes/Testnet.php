96cc571a7cc62dee0234849451291a03a212957e
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-02-25 18:34:35 +0500

    testnet node

---
+++ src/Nodes/Testnet.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is a part of "cryptolabs-pk/bitcoin-php" package.
+ * https://github.com/cryptolabs-pk/bitcoin-php
+ *
+ * Copyright (c) 2019 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/cryptolabs-pk/bitcoin-php/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace CryptoLabs\Bitcoin\Nodes;
+
+/**
+ * Class Testnet
+ * @package CryptoLabs\Bitcoin\Nodes
+ */
+class Testnet extends AbstractCryptoNode
+{
+    public const NETWORK_ID = "bitcoin-testnet";
+}
